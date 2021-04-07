3af1b0b0fc18b0850fb8ee4df075ae2f12ca6abb
pedro <pedro.frauamar@gmail.com>
2021-04-07 13:44:01 +0200

    Adding magnifying glass (pending corrections) and setting up agent information

--- js/annotator.js
+++ js/annotator.js
@@ -13,7 +13,7 @@ function toggleAccordionItem(accordionItem){
 function collapseAllButThis(element){
     var listOfCollapsableElems = [];
 
-    $("#accordionExample >> div").each((index, elem) => {
+    $("#agentsAccordion >> div").each((index, elem) => {
         listOfCollapsableElems.push(elem.id);
     });
 
@@ -32,7 +32,7 @@ function coordPercentages(x, y){
 }
 
 function loadJSONData(){
-    const JSONPath = "../annotations_json/anno_val/frankfurt_000000_012868_leftImg8bit_annotation.json";
+    const JSONPath = "../annotations_json/anno_train/strasbourg_000000_004383_leftImg8bit_annotation.json";
     var jsonObj = {};
 
     $.ajax({
@@ -47,22 +47,19 @@ function loadJSONData(){
     return jsonObj;
 }
 
-function loadCanvas(jsonData){
+function loadCanvas(jsonData, img, canvasElem){
     var canvas = document.getElementById('imgToAnnotate'),
     context = canvas.getContext('2d');
 
-    make_base(context, jsonData);
+    make_base(context, jsonData, img, canvasElem);
 }
 
-function make_base(context, jsonData)
+function make_base(context, jsonData, img, canvasElem)
 {
-    base_image = new Image();
-    base_image.src = '../img/val/frankfurt/frankfurt_000000_012868_leftImg8bit.png';
-    canvasElem = document.getElementById('imgToAnnotate');
-    base_image.onload = function(){
-        canvasElem.width = base_image.width;
-        canvasElem.height = base_image.height;
-        context.drawImage(base_image, 0, 0, canvasElem.width, canvasElem.height);
+    img.onload = function(){
+        canvasElem.width = img.width;
+        canvasElem.height = img.height;
+        context.drawImage(img, 0, 0, canvasElem.width, canvasElem.height);
 
         for(i = 0; i < Object.keys(jsonData.bbs).length; i++){
             var agent = Object.keys(jsonData.bbs)[i];
@@ -77,36 +74,130 @@ function make_base(context, jsonData)
     }
 }
 
+function loadAgents(jsonData){
+    var agentsAccordion = document.getElementById("agentsAccordion");
+    const classLabels = {
+        0: "Ignore",
+        1: "Pedestrian",
+        2: "Rider",
+        3: "Sitting person",
+        4: "Person in unusual posture",
+        5: "Group of people"
+    };
+
+    for(i = 0; i < Object.keys(jsonData.bbs).length; i++){
+        var accordionItem = document.createElement("div");
+        var accordionHeader = document.createElement("h2");
+        var accordionButton = document.createElement("button");
+        var collapsableElement = document.createElement("div");
+        var accordionBody = document.createElement("div");
+        var agentIndex = i+1;
+
+        accordionItem.className = "accordion-item";
+
+        accordionHeader.className = "accordion-header"
+        accordionHeader.id = "heading" + agentIndex;
+        
+        accordionButton.className = "accordion-button collapsed";
+        accordionButton.type = "button";
+        accordionButton.innerText = "Agent " + agentIndex;
+        /*accordionButton.data-bs-toggle = "collapse";
+        accordionButton.data-bs-target = "#collapse" + agentIndex;
+        accordionButton.aria-expanded = "false";
+        accordionButton.aria-controls = "collapse" + agentIndex;*/
+
+        collapsableElement.className = "accordion-collapse collapse";
+        collapsableElement.id = "collapse" + agentIndex;
+        /*collapsableElement.aria-labelledby = "heading" + agentIndex;
+        collapsableElement.data-bs-parent = "#agentsAccordion"*/
+
+        var agent = Object.keys(jsonData.bbs)[i];
+        var classLabelNumber = jsonData.bbs[agent].class_label;
+        var classLabel = classLabels[classLabelNumber];
+
+        accordionBody.className = "accordion-body";
+        accordionBody.innerHTML = `<p class="mb-0">Current label<p>
+        <button type="button" class="btn btn-primary rounded-pill btn-sm" data-bs-toggle="button"><span class="font-weight-bold">` + classLabel + `</span></button>
+        <p class="mb-0">Age</p> 
+        <button type="button" class="btn btn-primary rounded-pill btn-sm" data-bs-toggle="button"><span class="font-weight-bold">Adult</span></button>
+        <button type="button" class="btn btn-primary rounded-pill btn-sm" data-bs-toggle="button"><span class="font-weight-bold">Kid</span></button>
+        <button type="button" class="btn btn-primary rounded-pill btn-sm" data-bs-toggle="button"><span class="font-weight-bold">Unknown</span></button>
+        <p class="mb-0 mt-1">Sex</p>
+        <button type="button" class="btn btn-primary rounded-pill btn-sm" data-bs-toggle="button"><span class="font-weight-bold">Male</span></button>
+        <button type="button" class="btn btn-primary rounded-pill btn-sm" data-bs-toggle="button"><span class="font-weight-bold">Female</span></button>
+        <button type="button" class="btn btn-primary rounded-pill btn-sm" data-bs-toggle="button"><span class="font-weight-bold">Unknown</span></button>`
+
+        accordionHeader.appendChild(accordionButton);
+        collapsableElement.appendChild(accordionBody);
+        accordionItem.appendChild(accordionHeader);
+        accordionItem.appendChild(collapsableElement);
+        agentsAccordion.appendChild(accordionItem);
+    }
+}
+
+function getAgentToDeploy(jsonData, relX, relY){
+    var agentToDeploy = 0;
+    var canvasWidth = $('#imgToAnnotate').width();
+    var canvasHeight = $('#imgToAnnotate').height();
+    const imgOriginalWidth = 2048;
+    const imgOriginalHeight = 1024;
+    var percentageOfReductionWidth = canvasWidth/imgOriginalWidth;
+    var percentageOfReductionHeight = canvasHeight/imgOriginalHeight;
+    for(i = 0; i < Object.keys(jsonData.bbs).length; i++){
+        var agent = Object.keys(jsonData.bbs)[i];
+        var x = jsonData.bbs[agent].x1;
+        var y = jsonData.bbs[agent].y1;
+        var bBoxWidth = jsonData.bbs[agent].w;
+        var bBoxHeight = jsonData.bbs[agent].h;
+        var xCoordBottomRight = x + bBoxWidth;
+        var yCoordBottomRight = y + bBoxHeight;
+
+        if(relX > x*percentageOfReductionWidth && relX < xCoordBottomRight*percentageOfReductionWidth && 
+            relY > y*percentageOfReductionHeight && relY < yCoordBottomRight*percentageOfReductionHeight){
+                agentToDeploy = i + 1;
+                break;
+        }
+    }
+
+    return agentToDeploy;
+}
+
 $(document).ready(function() {
-    var jsonData = loadJSONData();
-    loadCanvas(jsonData);
+    var jsonData = loadJSONData();  
+    img = new Image();
+    img.src = '../img/train/strasbourg/strasbourg_000000_004383_leftImg8bit.png';
+    var canvasElem = document.getElementById('imgToAnnotate');
+    var zoom = document.getElementById("zoomed-canvas");
+    var zoomCtx = zoom.getContext("2d");
+    loadCanvas(jsonData, img, canvasElem);
+    loadAgents(jsonData);
+   /* $('#canvasContainer').mousemove(function(e){
+        var cursorX = e.pageX - $(this).offset().left;
+        var cursorY = e.pageY - $(this).offset().top;
+        var zoomFactor = 2;
+        var bw = 3;
+        var w = zoom.offsetWidth / 2;
+        var h = zoom.offsetHeight / 2;
+        zoomCtx.fillStyle = "transparent";
+        zoomCtx.fillRect(0,0, zoom.width, zoom.height);
+        //zoomCtx.drawImage(canvasElem, cursorX, cursorY, canvasElem.width, canvasElem.height, 0, 0, zoomFactor*canvasElem.width, zoomFactor*canvasElem.height);
+        zoom.style.backgroundRepeat = "no-repeat";
+        //zoom.style.backgroundSize = (canvasElem.width * zoomFactor) + "px " + (canvasElem.height * zoomFactor) + "px";
+        zoom.style.top = cursorY - h + "px";
+        zoom.style.left = cursorX - w + "px";
+        zoom.style.display = "block";
+        //zoom.style.backgroundPosition = "-" + ((cursorX * zoomFactor) - w + bw) + "px -" + ((cursorY * zoomFactor) - h + bw) + "px";
+    });*/
+    $('#imgToannotate').mouseout(function(){
+        zoom.style.display = "none";
+    });
     $("#imgToAnnotate").click(function(event){            
         var relX = event.pageX - $(this).offset().left;
         var relY = event.pageY - $(this).offset().top;
-        
-        /*
-        * For test purposes, those value are hardcoded for current hardcoded img.
-        */
-        var minXOne = 28.75;
-        var maxXOne = 38.46;
-        var minYOne = 37.36;
-        var maxYOne = 74.45;
-        var minXTwo = 62.82;
-        var maxXTwo = 73.26;
-        var minYTwo = 38.74;
-        var maxYTwo = 77.75;
-        
-        var clickedPointInPercentages = new coordPercentages(relX, relY);
-        
-        if(clickedPointInPercentages.X > minXOne && clickedPointInPercentages.X < maxXOne && 
-            clickedPointInPercentages.Y > minYOne && clickedPointInPercentages.Y < maxYOne){
-            collapseAllButThis('collapseOne');
-            toggleAccordionItem('collapseOne');
-        }
-        else if(clickedPointInPercentages.X > minXTwo && clickedPointInPercentages.X < maxXTwo && 
-            clickedPointInPercentages.Y > minYTwo && clickedPointInPercentages.Y < maxYTwo){
-            collapseAllButThis('collapseTwo');
-            toggleAccordionItem('collapseTwo');
-        }
+        var agentNumber = getAgentToDeploy(jsonData, relX, relY);
+        var collapsableElement = "collapse" + agentNumber;
+
+        collapseAllButThis(collapsableElement);
+        toggleAccordionItem(collapsableElement);
     });
 });
