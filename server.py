44ee77b32b36dcc209afd20a472d21151b5d43d4
Pedro Frau <pedro.frauamar@gmail.com>
2022-05-03 09:22:12 +0200

    updating broadcast host ip

--- server.py
+++ server.py
@@ -409,5 +409,5 @@ def walk_error_handler(exception_instance):
     print("The specified path is incorrect or permission is needed")
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port='5000')
+    app.run(debug=True, host='0.0.0.0', port='5000')
 
